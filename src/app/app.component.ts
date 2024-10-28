607c36a33b66b2defe20413edf6d92083d5d09c8
bhardwajj03 <adi.bhardwaj09@gmail.com>
2024-10-28 11:10:50 +0530

    practice

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { TodosComponent } from "./myComponents/todos/todos.component";
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, TodosComponent],
+  imports: [RouterOutlet, TodosComponent,app-reactiveform],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
